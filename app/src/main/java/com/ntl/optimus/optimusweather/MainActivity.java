d594e45663e36a1e4c5d126763fecfb465a1db79
88luan <88luan@gmail.com>
2014-11-21 16:44:10 +0700

    3

--- app/src/main/java/com/ntl/optimus/optimusweather/MainActivity.java
+++ app/src/main/java/com/ntl/optimus/optimusweather/MainActivity.java
@@ -2,18 +2,32 @@ package com.ntl.optimus.optimusweather;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    public static final int REQUEST_CODE_LOCATION = 984327;
+    private SharedPreferences sharedPreferences;
+    private String cityId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Weather");
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        cityId = sharedPreferences.getString("cityid", "");
+        String city = sharedPreferences.getString("cityName","");
+        String country = sharedPreferences.getString("country","");
+
+        TextView textView = (TextView) findViewById(R.id.textViewCity);
+        textView.setText( city + ", " + country);
     }
 
 
@@ -37,9 +51,28 @@ public class MainActivity extends Activity {
         }
         else if (id == R.id.action_locate) {
             Intent intent = new Intent(getApplication(), SearchLocationActivity.class);
-            startActivity(intent);
+            startActivityForResult(intent, REQUEST_CODE_LOCATION);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode== REQUEST_CODE_LOCATION) {
+            if(resultCode == RESULT_OK){
+                cityId = sharedPreferences.getString("cityid", "");
+                String city = sharedPreferences.getString("cityName","");
+                String country = sharedPreferences.getString("country","");
+
+                TextView textView = (TextView) findViewById(R.id.textViewCity);
+                textView.setText(city + ", " + country);
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
     }
 }
--- app/src/main/java/com/ntl/optimus/optimusweather/SearchLocationActivity.java
+++ app/src/main/java/com/ntl/optimus/optimusweather/SearchLocationActivity.java
@@ -1,16 +1,23 @@
 package com.ntl.optimus.optimusweather;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.ntl.optimus.optimusweather.adapter.CityAdapter;
@@ -59,6 +66,17 @@ public class SearchLocationActivity extends Activity {
                 search(edt.getEditableText().toString());
             }
         });
+
+        cityListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int pos,
+                                    long id) {
+
+                City city = (City) parent.getItemAtPosition(pos);
+                showAlertDialog(city.getId(), city.getName(), city.getCountry());
+            }
+        });
     }
 
     private void search(String input) {
@@ -78,6 +96,66 @@ public class SearchLocationActivity extends Activity {
             }
         });
     }
+
+    //region Show Alert Dialog
+
+    public static class AlertDialogFragment extends DialogFragment {
+
+        public static AlertDialogFragment newInstance(String id, String city, String country) {
+            AlertDialogFragment frag = new AlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putString("id", id);
+            args.putString("city", city);
+            args.putString("country", country);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            final String id = getArguments().getString("id");
+            final String city = getArguments().getString("city");
+            final String country = getArguments().getString("country");
+
+            return new AlertDialog.Builder(getActivity(), android.R.style.Theme_Holo_Dialog)
+                    .setTitle("Your Location") //nếu không setTitle thì dialog sẽ không có thanh actionbar
+                    .setIcon(R.drawable.ic_action_place)
+                    .setMessage(city +", "+ country)
+                    .setPositiveButton("Agree",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int whichButton) {
+                                    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                                    SharedPreferences.Editor editor = sharedPref.edit();
+                                    editor.putString("cityid", id);
+                                    editor.putString("cityName", city);
+                                    editor.putString("country", country);
+                                    editor.commit();
+                                    Intent intent = new Intent();
+                                    getActivity().setResult(RESULT_OK, intent);
+                                    getActivity().finish();
+                                }
+                            }
+                    )
+                    .setNegativeButton("Disagree",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int whichButton) {
+                                    //do something
+                                }
+                            }
+                    )
+                    .create();
+        }
+    }
+
+    public void showAlertDialog(String id, String city, String country){
+        DialogFragment newFragment = AlertDialogFragment.newInstance(id,city,country);
+        newFragment.show(getFragmentManager(), "dialogAlert");
+    }
+
+    //endregion
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
