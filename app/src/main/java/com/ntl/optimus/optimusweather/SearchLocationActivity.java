c6d34bcf21e68ff58f95123c7c7dd971d121c794
88luan <88luan@gmail.com>
2014-11-21 11:07:02 +0700

    1
    add lib
    add search location activity

--- app/src/main/java/com/ntl/optimus/optimusweather/SearchLocationActivity.java
+++ app/src/main/java/com/ntl/optimus/optimusweather/SearchLocationActivity.java
@@ -1,25 +1,24 @@
 package com.ntl.optimus.optimusweather;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class SearchLocationActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search_location);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_search_location, menu);
         return true;
     }
 
@@ -33,10 +32,6 @@ public class MainActivity extends Activity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
-        else if (id == R.id.action_locate) {
-            Intent intent = new Intent(getApplication(), SearchLocationActivity.class);
-            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
